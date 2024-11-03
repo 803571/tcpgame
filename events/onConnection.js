506f8d8032c08ca37fdc1cfa5030fbae6a0d63b3
803571 <8omsut@naver.com>
2024-11-03 23:15:43 +0900

    3주차 3강 이벤트 구분, 바이트 배열 분해

--- events/onConnection.js
+++ events/onConnection.js
@@ -7,10 +7,9 @@ export const onConnection = (socket) => {
     console.log(`클라이언트가 연결되었습니다. :  ${socket.remoteAddress}: ${socket.remotePort}`);
 
     // 각 클라이언트마다 고유한 Buffer를 유지하기 위함
-    socket.buffer = buffer.alloc(0);
+    socket.buffer = Buffer.alloc(0);
 
     socket.on('data', onData(socket));
     socket.on('end', onEnd(socket));
     socket.on('error', onError(socket));
-    
-}
+};
--- events/onData.js
+++ events/onData.js
@@ -6,19 +6,24 @@ export const onData = (socket) => (data) => {
   socket.buffer = Buffer.concat([socket.buffer, data]);
 
   // 패킷의 총 헤더 길이 (패킷의 길이와 타입의 정보)
-  const totalHeaderLength = config.packet.totalHeaderLength + config.packet.typeLength;
+  const totalHeaderLength = config.packet.totalLength + config.packet.typeLength;
 
   // 버퍼에 최소한 전체 헤더가 있을 때만 패킷을 처리합니다.
   while (socket.buffer.length >= totalHeaderLength) {
     // 패킷 길이 정보 수신 4바이트
     const length = socket.buffer.readUInt32BE(0);
     // 패킷 길이 정보 수신 1바이트
-    const packetType = socket.buffer.readUInt8(config.packet.typeLength);
-    
+    const packetType = socket.buffer.readUInt8(config.packet.totalLength);
+
     // 패킷 전체 길이 확인 후 데이터를 수신
     if (socket.buffer.length >= length) {
-        // 패킷의 데이터를 자르고 버퍼에서 제거
-      console.log(`length: ${length}, packetType: ${packetType}`);
+      const packet = socket.buffer.slice(totalHeaderLength, length);
+      socket.buffer = socket.buffer.slice(length);
+
+      // 패킷의 데이터를 자르고 버퍼에서 제거
+      console.log(`length: ${length}`);
+      console.log(`packetType: ${packetType}`);
+      console.log(`packet: ${packet}`);
     } else {
       // 전체 패킷이 아직 도착하지 않았다면
       break;
